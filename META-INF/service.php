3c850f9d39e3d8c97385e19533e71d1451b5a518
xiaohengjin <xiaohengjin@youzan.com>
2017-09-01 18:16:09 +0800

    TcpServer instance bind

--- META-INF/service.php
+++ META-INF/service.php
@@ -1,3 +1,11 @@
 <?php
 
+use ZanPHP\Support\Di;
+
+$container = \ZanPHP\Container\Container::getInstance();
+
+$container->bind("ServerBase.TcpServer", function ($_, $args) {
+    return Di::make(\ZanPHP\TcpServer\Server::class, [$args[0], $args[1]]);
+});
+
 return [];
